b549eb731db416b141219e14b4ff974a8512ddb9
Scruffy pc <cayabyabrr@yahoo.com>
2025-07-13 19:52:59 -0500

    Testing simple code generation

--- src/main.py
+++ src/main.py
@@ -7,17 +7,35 @@ OLLAMA_ENDPOINT = f"{OLLAMA_URL}/api/generate"
 client = ollama.Client()
 
 model = "deepseek-r1"
-prompt = "Hello world!"
+prompt = """
+    Generate just code in selenium python (no explanations or notes) based on this cucumber feature file, website used is saucedemo.com:
+    Feature: User can add item to cart
+    
+        Scenario:
+            Given I log in
+            When I add a backpack to the cart
+            Then the cart icon should show "1"
+    """
 
 response = client.generate(
     model=model, 
     prompt=prompt, 
-    stream=True
+    stream=True,
+    think=False,
+    # raw=True,
+    options={
+        # "num_predict": 100,
+        "temperature": 0.2,
+        # "stop": ["```"]
+    }
     )
 
 # print(response.response)
 # print(json.dumps((response.response), indent=2))
 
 for chunk in response:
-    # Print each partial response string as it comes, without newline
-    print(chunk.response, end="", flush=False)
+    print(
+        chunk.response, 
+        end="", 
+        flush=False
+        )
